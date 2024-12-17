193127a9f0877c09ec9049de2364a925352798e9
Alex <worldgoggo05@gmail.com>
2024-12-18 05:27:01 +0900

    fix : restaurant login and signup process

--- src/models/Member.service.ts
+++ src/models/Member.service.ts
@@ -3,6 +3,7 @@ import { LoginInput, Member, MemberInput } from "../libs/types/member";
 import { HttpCode, Message } from "../libs/Errors";
 import Errors from "../libs/Errors";
 import { MemberType } from "../libs/enums/member.enum";
+import * as bcrypt from "bcryptjs";
 
 class MemberService {
     private readonly memberModel;
@@ -17,6 +18,13 @@ class MemberService {
         if(exist) {
             throw new Errors(HttpCode.BAD_REQUEST, Message.CREATE_FAILED);
         }
+
+        // Password hashing
+        // console.log("Before: ", input.memberPassword);
+        const salt = await bcrypt.genSalt();
+        input.memberPassword = await bcrypt.hash(input.memberPassword, salt);
+        // console.log("After: ", input.memberPassword);
+
        try {
         const result = await this.memberModel.create(input);
         result.memberPassword = "";
@@ -37,8 +45,11 @@ class MemberService {
         if(!member) {
             throw new Errors(HttpCode.NOT_FOUND, Message.NO_MEMBER_NICK);
         }
-
-        const isMatch = input.memberPassword === member.memberPassword;
+        const isMatch = await bcrypt.compare(
+            input.memberPassword, 
+            member.memberPassword
+        );
+        // const isMatch = input.memberPassword === member.memberPassword;
         if(!isMatch) {
             throw new Errors(HttpCode.UNAUTHORIZED, Message.WRONG_PASSWORD);
         }
