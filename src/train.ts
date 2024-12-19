13baedae3e7aefd4f5629c3909b3bb9c8e004d76
Alex <worldgoggo05@gmail.com>
2024-12-20 05:23:20 +0900

    K-Task Done

--- src/train.ts
+++ src/train.ts
@@ -1,16 +1,27 @@
-// J-Task
-// Shunday function yozing, u string qabul qilsin va string ichidagi eng uzun sozni qaytarsin.
-//MASALAN: findLongestWord("I come from Uzbekistan") return "Uzbekistan"
+// K-TASK: 
 
-function findLongestWord(str:string){
-    let arr:string[] = str.split(' ')
-    let longW:string= ""
+// Shunday function yozing, u string qabul qilsin va string ichidagi unli harflar sonini qaytarsin.
+// MASALAN: countVowels("string") return 1;
 
-    for(let char of arr){
-        if(char.length> longW.length){
-            longW=char
-        }
-        
-    }return longW
+
+//1-Usul
+function countVl(str:string){
+    return str.toLowerCase().split('').filter(char => 'aeiou'.includes(char)).length
 }
-console.log(findLongestWord("I come from Uzbekistan"))
+
+// console.log(countVl("stringaa"))
+
+
+//2-Usul
+const unli = (str:string) => {
+    let vl = ['a','e','u','i','o']
+    let st = str.toLowerCase()
+    let count = 0
+    for(let i of str){
+        if(vl.includes(i)){
+            count++
+        }
+    }return count
+}
+
+console.log(unli('straing'))
