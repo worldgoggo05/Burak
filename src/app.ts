8180a6dff34f2114bca383640ae4041a0a336a75
Alex <worldgoggo05@gmail.com>
2024-12-23 06:37:00 +0900

    feat: Sessions via middleware

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,16 @@ import routerAdmin from "./router-admin"
 import morgan from "morgan"
 import { MORGAN_FORMAT } from "./libs/config"
 
+import session from "express-session";
+import ConnectMongoDB from "connect-mongodb-session";
+
+const MongoDBStore = ConnectMongoDB (session);
+const store = new MongoDBStore({
+    uri: String (process.env.MONGO_URL),
+collection: "sessions",
+});
+
+
 /** 1-ENTRANCE **/
 const app = express();
 console.log("__dirname", __dirname);
@@ -15,6 +25,19 @@ app.use(express.json());
 app.use(morgan(MORGAN_FORMAT));
 
 /** 2-SESSIONS **/
+
+app.use (
+    session ({
+        secret: String (process.env.SESSION_SECRET),
+        cookie: {
+            maxAge: 1000 * 3600 * 3 // 3 Hours
+        },
+        store: store,
+        resave: true, // Save session even if it's modified (true)
+        saveUninitialized: true // Save session even if it's not modified (true)
+    })
+);
+
 
 /** 3-VIEWV **/
 app.set("views", path.join(__dirname, "views"));
