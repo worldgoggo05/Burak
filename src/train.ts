3430a15ef5f7dbe64f544bb949170d53b587112a
Alex <worldgoggo05@gmail.com>
2024-12-24 05:34:20 +0900

    L-Task Done

--- src/train.ts
+++ src/train.ts
@@ -1,27 +1,23 @@
-// K-TASK: 
+// L-Task
+// Shunday function yozing, u string qabul qilsin va string ichidagi hamma sozlarni chappasiga yozib va sozlar ketma-ketligini buzmasdan stringni qaytarsin.
+// MASALAN: reverseSentence("we like coding") return "ew ekil gnidoc";
 
-// Shunday function yozing, u string qabul qilsin va string ichidagi unli harflar sonini qaytarsin.
-// MASALAN: countVowels("string") return 1;
 
-
-//1-Usul
-function countVl(str:string){
-    return str.toLowerCase().split('').filter(char => 'aeiou'.includes(char)).length
+//1
+function revSen(str:string){
+    let st = str.split(' ')
+    return st.map(a=> a.split('').reverse().join('')).join(' ')
 }
 
-// console.log(countVl("stringaa"))
+console.log(revSen("we like coding"))
 
 
-//2-Usul
-const unli = (str:string) => {
-    let vl = ['a','e','u','i','o']
-    let st = str.toLowerCase()
-    let count = 0
-    for(let i of str){
-        if(vl.includes(i)){
-            count++
-        }
-    }return count
+//2
+function revSent(str:string):string{
+    return str
+        .split(' ')
+        .reduce((acc,val )=> acc+ ' ' +[...val].reverse().join(''), '')
+        .trim()
 }
 
-console.log(unli('straing'))
+// console.log(revSent("we like coding"))
