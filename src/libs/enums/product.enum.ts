72dc2c4ca99c158ef311e48d617f56cd0b45b14e
Alex <worldgoggo05@gmail.com>
2024-12-25 04:40:42 +0900

    feat: configure Product Schema Model

---
+++ src/libs/enums/product.enum.ts
@@ -0,0 +1,28 @@
+export enum ProductSize {
+    SMALL = 'SMALL',
+    NORMAL = 'NORMAL',
+    LARGE = 'LARGE',
+    SET = 'SET'
+}
+
+export enum ProductVolume {
+    HALF = '0.5L',
+    ONE = '1L',
+    ONE_POINT_TWO = '1.2L',
+    ONE_POINT_FIVE = '1.5L',
+    TWO = '2L',
+}
+
+export enum ProductStatus {
+    PAUSE = "PAUSE",
+    PROCESS = "PROCESS",
+    DELETE = "DELETE",
+}
+
+export enum ProductCollection {
+    DISH = "DISH",
+    SALAD = "SALAD",
+    DESSERT = "DESERT",
+    DRINK = "DRINK",
+    OTHER = "OTHER",
+}
